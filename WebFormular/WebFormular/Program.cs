d44d947585dd8affae17a9b333d6a46a1fd6f627
condradow <condrado.warken@icloud.com>
2026-08-27 23:30:56 +0200

    MudBlazor hinzugefügt

--- WebFormular/WebFormular/Program.cs
+++ WebFormular/WebFormular/Program.cs
@@ -1,12 +1,15 @@
+using MudBlazor.Services;
 using WebFormular.Components;
-
+using MudBlazor.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddMudServices();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())

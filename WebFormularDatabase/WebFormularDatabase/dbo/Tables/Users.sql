60923e1dd6995cfcf816d05cc434896c7a421c25
condradow <condrado.warken@icloud.com>
2026-08-29 09:20:21 +0200

    Datenbank hinzugefügt

---
+++ WebFormularDatabase/WebFormularDatabase/dbo/Tables/Users.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]           UNIQUEIDENTIFIER NOT NULL,
+    [Username]     NVARCHAR (50)    NULL,
+    [Password]     NVARCHAR (50)    NULL,
+    [PasswordHash] NVARCHAR (50)    NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+

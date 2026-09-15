baf75486723fc612bfa620de94c119ebe11e5321
condradow <condrado.warken@icloud.com>
2026-09-15 21:47:45 +0200

    Darstellung überarbeitet von 2.1

--- WebFormular/Classes/Elements/TeachingAbsenceReason/TeachingAbsenceReason.cs
+++ WebFormular/Classes/Elements/TeachingAbsenceReason/TeachingAbsenceReason.cs
@@ -2,10 +2,10 @@
 {
     public class TeachingAbsenceReason : ElementBase
     {
-        public DateTime BeginAbsence { get; set; }
-        public DateTime EndAbsence { get; set; }
+        public DateTime? BeginAbsence { get; set; }
+        public DateTime? EndAbsence { get; set; }
         public Boolean MaterialOnTeachingPlatform { get; set; }
-        public Boolean MaterialOnLearningPlatform { get; set; }
+        public Boolean MaterialOnDemand { get; set; }
         public Boolean MaterialOnMail { get; set; }
     }
 }

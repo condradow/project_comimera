d3ec87f2ca6403c5b62d94d13dd0cb9707c88018
condradow <condrado.warken@icloud.com>
2026-09-16 20:36:53 +0200

    Signature Elemente hinzugefügt

--- WebFormular/Classes/Elements/OfficialDutyDuringStudentAbsence/OfficialDutyDuringStudentAbsenceClarification.cs
+++ WebFormular/Classes/Elements/OfficialDutyDuringStudentAbsence/OfficialDutyDuringStudentAbsenceClarification.cs
@@ -1,11 +1,17 @@
-﻿namespace WebFormular.Classes.Elements.OfficialDutyDuringStudentAbsence
+﻿using System.Security.Cryptography.Xml;
+
+namespace WebFormular.Classes.Elements.OfficialDutyDuringStudentAbsence
 {
     public class OfficialDutyDuringStudentAbsenceClarification
     {
         public DateTime? Date { get; set; }
         public int? TeachingPeriod { get; set; }
         public string OtherOfficialDuty { get; set; } = string.Empty;
-        public string Base64SignatureTeacher { get; set; }= string.Empty;
-        public string Base64SignatureDepartmentHead { get; set; } = string.Empty;
+        public byte[] SignatureTeacher { get; set; } = [];
+        public byte[] SignatureDepartmentHead { get; set; } = [];
+        public string Base64SignatureTeacher => System.Text.Encoding.UTF8.GetString(SignatureTeacher);
+        public string Base64SignatureDepartmentHead => System.Text.Encoding.UTF8.GetString(SignatureDepartmentHead);
+
+
     }
 }

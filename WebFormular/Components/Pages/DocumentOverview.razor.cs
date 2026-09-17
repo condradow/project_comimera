2fedeb46931558effd0231d2d5a35b5718a941f3
condradow <condrado.warken@icloud.com>
2026-09-17 06:46:09 +0200

    Rider anpassung

---
+++ WebFormular/Components/Pages/DocumentOverview.razor.cs
@@ -0,0 +1,53 @@
+﻿using WebFormular.Classes;
+
+namespace WebFormular.Components.Pages;
+
+public partial class DocumentOverview
+{
+    private List<DocumentBase> Documents { get; set; } = [];
+    private string SearchString { get; set; } = string.Empty;
+
+    private Func<DocumentBase, bool> QuickFilter => document =>
+    {
+        if (string.IsNullOrWhiteSpace(SearchString))
+            return true;
+
+        if (document.Title?.Contains(
+                SearchString,
+                StringComparison.OrdinalIgnoreCase) == true)
+            return true;
+
+        if (document.Description?.Contains(
+                SearchString,
+                StringComparison.OrdinalIgnoreCase) == true)
+            return true;
+
+        if (document.CreatedBy?.Contains(
+                SearchString,
+                StringComparison.OrdinalIgnoreCase) == true)
+            return true;
+
+        if (document.ModifiedBy?.Contains(
+                SearchString,
+                StringComparison.OrdinalIgnoreCase) == true)
+            return true;
+
+        return false;
+    };
+
+    private async Task DeleteDocument(DocumentBase document)
+    {
+        // Später DatabaseService aufrufen
+
+        // await Database.DeleteDocumentAsync(document.Id);
+
+        Documents.Remove(document);
+
+        await InvokeAsync(StateHasChanged);
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        this.Documents = await DbContext.GetAllAsync();
+    }
+}
